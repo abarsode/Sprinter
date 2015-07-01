918cf9dd0bd697a496be02b53a7130463f874021
Alok Barsode <alok.barsode@gmail.com>
2015-07-01 16:28:47 +0100

    Create a home Router
    Create an express Router using express.Router() for the homepage(/).
    Add /login and /about routes to the router. Add the router to the
    application.

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 //Main server file
+var express = require('express');
+
+//Created a express applicatiom
+var app = express();
+
+var homeRoute = express.Router();
+
+homeRoute.get('/', function(req, res){
+	res.send("You have reached the home page");
+});
+
+homeRoute.get('/login', function(req, res){
+	res.send("You have reached the login page");
+});
+
+homeRoute.get('/about', function(req, res){
+	res.send("You have reached the About page");
+});
+
+app.use('/',homeRoute);
+
+app.listen(4000);
+console.log('Server running on port 4000');
