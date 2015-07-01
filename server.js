440d885ffb266dd236f759dfb38f5ffcc332d5a2
Alok Barsode <alok.barsode@gmail.com>
2015-07-01 16:44:38 +0100

    Use middleware to intercept requests
    Using Router.use() middleware to intercept incoming requests. This is
    helpful in doing tasks like logging, parameter checking fore the
    request is processed. Just logging the home and admin requests in this
    commit.

--- server.js
+++ server.js
@@ -4,10 +4,26 @@ var express = require('express');
 //Created a express applicatiom
 var app = express();
 
+//Define our routes here.
 var homeRoute = express.Router();
 var adminRoute = express.Router();
 var userRoute = express.Router();
 
+// Add Route middleware. This will be invoked before each call
+// to the home route. This is useful in intercepting the requests
+// and processing the request before the route.
+
+homeRoute.use(function(req, res, next){
+	console.log("HOME-METHOD:" + req.method + " URL:" + req.url);
+	next();
+});
+
+adminRoute.use(function(req, res, next){
+	console.log("ADMIN-METHOD:" + req.method + " URL:" + req.url);
+	next();
+});
+
+//Setup the routes
 homeRoute.get('/', function(req, res){
 	res.send("You have reached the home page");
 });
@@ -36,6 +52,7 @@ userRoute.get('/projects', function(req, res){
 	res.send("You have reached user's Projects page");
 });
 
+//Add the routes to the application
 app.use('/',homeRoute);
 app.use('/admin', adminRoute);
 app.use('/user', userRoute);
