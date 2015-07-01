6b2c0a9eefd1e7ddc875870f9c19410387773357
Alok Barsode <alok.barsode@gmail.com>
2015-07-01 16:35:23 +0100

    Add more routes
    Add admin and user routers. Create /admin/projects and /user/projects
    routes.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require('express');
 var app = express();
 
 var homeRoute = express.Router();
+var adminRoute = express.Router();
+var userRoute = express.Router();
 
 homeRoute.get('/', function(req, res){
 	res.send("You have reached the home page");
@@ -18,7 +20,25 @@ homeRoute.get('/about', function(req, res){
 	res.send("You have reached the About page");
 });
 
+adminRoute.get('/', function(req, res){
+	res.send("You have reached admin page");
+});
+
+adminRoute.get('/projects', function(req, res){
+	res.send("You have reached admin's Projects page");
+});
+
+userRoute.get('/', function(req, res){
+	res.send("You have reached user page");
+});
+
+userRoute.get('/projects', function(req, res){
+	res.send("You have reached user's Projects page");
+});
+
 app.use('/',homeRoute);
+app.use('/admin', adminRoute);
+app.use('/user', userRoute);
 
 app.listen(4000);
 console.log('Server running on port 4000');
