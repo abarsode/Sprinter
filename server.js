cd9be377927d1aa83544ce6ce5aaec7650dfd084
Alok Barsode <alok.barsode@gmail.com>
2015-07-01 17:23:43 +0100

    Process get request parameters
    Use req.params. to get the parameters from the GET request. Here we
    test the ‘name’ of the project starts with an alphabet, otherwise we
    send status 400(Bad request).

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 var homeRoute = express.Router();
 var adminRoute = express.Router();
 var userRoute = express.Router();
+var projectRoute = express.Router();
 
 // Add Route middleware. This will be invoked before each call
 // to the home route. This is useful in intercepting the requests
@@ -52,10 +53,24 @@ userRoute.get('/projects', function(req, res){
 	res.send("You have reached user's Projects page");
 });
 
+projectRoute.get('/', function(req, res){
+	res.sendStatus(404);
+});
+
+projectRoute.get('/:name', function(req, res){
+	var name = req.params.name;
+	var regex = /^[A-Za-z]*$/;
+	if (regex.test(name))
+		res.send("You have reached Project:" + name + " page");
+	else
+		res.sendStatus(400);
+});
+
 //Add the routes to the application
 app.use('/',homeRoute);
 app.use('/admin', adminRoute);
 app.use('/user', userRoute);
+app.use('/project', projectRoute);
 
 app.listen(4000);
 console.log('Server running on port 4000');
